16a170b36fb932595a1fa5db69adbd75154cb95f
Oslynx <malakayalvg@gmail.com>
2024-02-13 16:05:50 +0100

    done

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(Request $request): Response
+    {
+        $prenom = $request->get("prenom");
+
+
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'prenom'=>$prenom
+        ]);
+    }
+}
